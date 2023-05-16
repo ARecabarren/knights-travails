b645eac23892daf9f068eeb40be505f9c001a883
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-05-16 13:11:55 -0400

    Test object and matrix

--- main.rb
+++ main.rb
@@ -1,4 +1,10 @@
 require_relative('./lib/knight')
 
-dark_knight = Knight.new([3,3])
+board = GameBoard.new(8,8)
+
+dark_knight = Knight.new([3,3], board)
 p dark_knight.edge_list 
+puts ' 0--1--2--3--4--5--6--7'
+dark_knight.adjancency_matrix.each do |line|
+    p line
+end
