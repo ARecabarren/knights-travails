3237bbf77dcd3356e6842d7e13a4491530f37129
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-05-17 12:57:58 -0400

    Test path resultant

--- main.rb
+++ main.rb
@@ -3,8 +3,12 @@ require_relative('./lib/knight')
 board = GameBoard.new(8,8)
 
 dark_knight = Knight.new([3,3], board)
-p dark_knight.edge_list 
-puts ' 0--1--2--3--4--5--6--7'
-dark_knight.adjancency_matrix.each do |line|
-    p line
-end
+# p dark_knight.edge_list 
+# puts ' 0--1--2--3--4--5--6--7'
+# dark_knight.adjancency_matrix.each do |line|
+#     p line
+# end
+
+p dark_knight.shortest_path([0,0],[3,3])
+p dark_knight.shortest_path([3,3],[3,3])
+p dark_knight.shortest_path([0,0],[1,2])
